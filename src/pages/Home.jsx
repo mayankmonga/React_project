7c7bb902f18114fb9b8cb61deb2c6662b7dedaa6
Mayank Monga <mayankmonga31@gmail.com>
2026-07-09 17:24:33 +0530

    made changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 import Hero from '../components/Hero/Hero';
-import Features from '../components/Features/Features';
+import Features from '../components/features/Features';
 import AboutPreview from '../components/About/AboutPreview';
 import CategoriesPreview from '../components/Categories/Categoriespreview';
 import BestSeller from '../components/Products/BestSeller';
