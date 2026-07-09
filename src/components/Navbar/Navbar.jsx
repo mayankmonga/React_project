c10522a394d227322ad55f138662957775250db7
Mayank Monga <mayankmonga31@gmail.com>
2026-07-09 17:04:20 +0530

    added hammburger mneu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,24 +5,30 @@ import Search from "../../assets/images/search.svg";
 import Profile from "../../assets/images/profile.svg";
 import Kart from "../../assets/images/kart.svg";
 import Logo from "../../assets/images/logo.jpg";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
 
-
+    const [menuOpen, SetmenuOpen]=useState(false);
 
     return (
         <nav className="navbar">
+            
             <div className="navbar-container">  
+                <div className="menu-icon" onClick={() => SetmenuOpen(!menuOpen)}>
+                    {menuOpen ? <FaTimes /> : <FaBars />}
+                </div>
                 <div className="navbar-left">
                     <div className="navbar-logo">
                         <img src={Logo} alt="Vividkraft Logo" />
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <ul className="navbar-menu">
+                    <ul className={`navbar-menu, ${menuOpen ? "nav-links active" : "nav-links"}`}>
                         {menuItems.map((item) => (
                             <li key={item.id}>
-                                <Link to={item.path} className="navbar-link">
+                                <Link to={item.path} onClick={() => SetmenuOpen(false)} className="navbar-link">
                                     {item.name}
                                 </Link>
                             </li>
